0d7c9a0360e92bdfdfac81961ffd55a26137b001
SubVampire Slayer <subvampireslayer@example.com>
2026-07-20 23:18:35 +0530

    Solved LeetCode 32

---
+++ STACK/LVC.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.length();
+        stack<int> st;
+        st.push(-1);
+        int maxlen = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '(') {
+                st.push(i);
+            }
+            else { // ch == ')'
+                 st.pop();
+                 if(st.empty()) st.push(i);
+                 else maxlen = max(maxlen , i - st.top());
+                } 
+            }
+        return maxlen;
+    }
+};
