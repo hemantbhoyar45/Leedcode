5a0687f2daf999dbf76673c24301c4886e0010c9
hemantbhoyar45 <hemantbhoyar585@gmail.com>
2026-08-27 17:40:05 +0530

    Time: 481 ms (87.28%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,7 @@
+select 
+activity_date as day,
+COUNT(DISTINCT user_id) as active_users 
+from Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+group by activity_date 
+ORDER BY activity_date ASC;
